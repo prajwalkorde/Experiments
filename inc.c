a51801828fe9a356fd677ebb75a8b0341ef61d4b
Prajwal korde <prajwalrk3071@gmail.com>
2025-09-09 17:39:52 +0530

    Add files via upload

---
+++ inc.c
@@ -0,0 +1,14 @@
+// #include <stdio.h>
+// void main(){
+// int a = 20;
+// printf("%d %d %d", a, ++a, a++);
+// } 22 22 20
+
+// #include <stdio.h>
+
+// void main() {
+//     int a = 20;
+//     int b = ++a;
+//     int c = a++;
+//     printf("%d %d %d\n", a, b, c);
+// } 22 21 21
